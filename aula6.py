e68984018f153902033ce007d8335a0cc6d266c6
Jonathan <jonathandscoutinho@gmail.com>
2025-07-23 22:51:45 -0300

    aula6

---
+++ aula6.py
@@ -0,0 +1,15 @@
+# Estrutura de COntrole de Fluxo - Loops (While/For)
+
+# While Loop
+
+x = 0
+while True:
+    x += 1
+    print(f"Loop infinito: {x}")
+
+print("----------------")
+
+# For Loop
+
+for x in range(1, 1000000):
+    print(f"Valor de x: {x}")
